68d91c741023190c5de2603f2b28dfdd8f561d3f
baibing <baibing@rongcloud.cn>
2019-05-07 19:07:50 +0800

    change project name

---
+++ src/app/chat/shared/services/rest.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable()
+export class RestService {
+
+  constructor() { }
+
+}
