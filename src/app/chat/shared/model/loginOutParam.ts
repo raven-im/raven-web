2a893c121856d5b9cdccb90360c1b70ee9372159
baibing <baibing@rongcloud.cn>
2019-05-07 20:31:11 +0800

    http client done.

---
+++ src/app/chat/shared/model/loginOutParam.ts
@@ -0,0 +1,5 @@
+export class LoginOutParam {
+    appKey: number;
+    uid: string;
+    token: string;
+}
---
+++ src/app/chat/shared/model/restResult.ts
@@ -0,0 +1,7 @@
+import { LoginOutParam } from "./loginOutParam";
+
+export class RestResult {
+    code: number;
+    msg: string;
+    data: LoginOutParam;
+}
