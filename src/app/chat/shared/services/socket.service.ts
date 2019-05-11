a1366a3e8df524a044a59b307de400d50110533a
baibing <baibing@rongcloud.cn>
2019-05-11 16:28:45 +0800

    pb decode done.

--- src/app/chat/shared/services/socket.service.ts
+++ src/app/chat/shared/services/socket.service.ts
@@ -24,10 +24,10 @@ export class SocketService {
 	}
 
     private create(url): Rx.Subject<MessageEvent> {
-		let ws = new WebSocket(url);
-
+        let ws = new WebSocket(url);
 		let observable = Rx.Observable.create(
 			(obs: Rx.Observer<MessageEvent>) => {
+                ws.binaryType = 'arraybuffer';
 				ws.onmessage = obs.next.bind(obs);
 				ws.onerror = obs.error.bind(obs);
 				ws.onclose = obs.complete.bind(obs);
@@ -37,8 +37,8 @@ export class SocketService {
 		let observer = {
 			next: (data: com.raven.common.protos.RavenMessage) => {
 				if (ws.readyState === WebSocket.OPEN) {
-                    let binary: Uint8Array = com.raven.common.protos.RavenMessage.encodeDelimited(data).finish();
-					ws.send(binary.buffer);
+                    let binary: Uint8Array = com.raven.common.protos.RavenMessage.encode(data).finish();
+					ws.send(binary);
 				}
 			}
 		}
@@ -46,44 +46,40 @@ export class SocketService {
 		return Rx.Subject.create(observer, observable);
     }
     
-    
     public initSocket(): void {
         // read from localstorage for socket connection.
         let url = localStorage.getItem('access-node');
+        let that = this;
         this.messages = <Subject<com.raven.common.protos.RavenMessage>>this.connect("ws://" + url + "/ws")
 			.pipe(map((response: MessageEvent): com.raven.common.protos.RavenMessage => {
-                console.log("Response from websocket 22");
-				// let data = JSON.parse(response.data);
-				return null;
-					// new com.raven.common.protos.RavenMessage();
-					
-				
+                return com.raven.common.protos.RavenMessage.decode(new Uint8Array(response.data)); 
             }));
-        this.messages.subscribe(msg => {			
-            console.log("Response from websocket: " + msg);
+        this.messages.subscribe(msg => {
+            console.log("Response from websocket: " + msg.type);
         });
     }
 
     public login(): void {
-        let message = com.raven.common.protos.RavenMessage;
-        let msg:com.raven.common.protos.RavenMessage = message.create();
-        let login = com.raven.common.protos.Login;
-        let lg: com.raven.common.protos.Login = login.create();
+        
+        let login = com.raven.common.protos.Login.create({
+            id: this.clientId, 
+            uid: localStorage.getItem("user"),
+            token: localStorage.getItem("token")
+        });
 
-        lg.id = this.clientId++;
-        lg.uid = localStorage.getItem("user");
-        lg.token = localStorage.getItem("token");
-        msg.login = lg;
+        let message = com.raven.common.protos.RavenMessage.create({
+            type: com.raven.common.protos.RavenMessage.Type.Login,
+            login: login
+        });
 
-        this.msgMap[this.clientId] = msg;
-        this.sendMsg(msg);
+        this.msgMap[this.clientId++] = message;
+        this.sendMsg(message);
         this.isLogin = true;//TODO
     }
 
     sendMsg(msg: com.raven.common.protos.RavenMessage) {
         console.log('new message from client to websocket: ');
 		this.messages.next(msg);
-		// this.message.message = '';
 	}
     // private sendMsg(msg): void {
     //     if(this.socket.connected) {
