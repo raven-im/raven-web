460f154bb3893665745b3b59b20c8729a1ca01a0
baibing <baibing@rongcloud.cn>
2019-05-11 23:03:30 +0800

    PB message sent & Ack handle structure  done.

--- src/app/chat/shared/services/socket.service.ts
+++ src/app/chat/shared/services/socket.service.ts
@@ -85,20 +85,67 @@ export class SocketService {
     
     private handleIncomingMsg(msg: com.raven.common.protos.RavenMessage): void {
         console.log("Response from websocket: " + msg.type);
+        let original;
         switch(msg.type) {
             case com.raven.common.protos.RavenMessage.Type.LoginAck:
                 //check if local saved , otherwise invalid.
-                let ack = msg.loginAck;
-                if (ack.code === com.raven.common.protos.Code.SUCCESS && 
-                    this.msgMap.get(ack.id) != null) {
+                let loginAck = msg.loginAck;
+                if (loginAck.code === com.raven.common.protos.Code.SUCCESS && this.msgMap.has(loginAck.id)) {
                     this.isLogin = true;
+                    this.msgMap.delete(loginAck.id);
                 }
                 break;
             case com.raven.common.protos.RavenMessage.Type.HeartBeat:
                 let hb = msg.heartBeat;
                 this.sendPong(hb.id);
                 break;
-        }
+            case com.raven.common.protos.RavenMessage.Type.MessageAck:
+                let msgAck = msg.messageAck;
+                if (this.msgMap.has(msgAck.cid)) {
+                    original = this.msgMap.get(msgAck.cid);
+                } else {
+                    console.log("error: map not contain " + msgAck.cid);
+                } 
+                if (msgAck.code === com.raven.common.protos.Code.SUCCESS) {
+                    //TODO message sent success. add Original Msg to Chat List.
+                } else {
+                    console.log("error: message ack fail. ");
+                }
+                break;
+            case com.raven.common.protos.RavenMessage.Type.ConverAck:
+                let convAck = msg.converAck;
+                if (this.msgMap.has(convAck.id)) {
+                    original = this.msgMap.get(convAck.id);
+                } else {
+                    console.log("error: map not contain " + convAck.id);
+                } 
+                if (convAck.code === com.raven.common.protos.Code.SUCCESS) {
+                    if (convAck.converList != null) {
+                        // TODO Conversation List.
+                    } else if (convAck.converInfo.converId != null) {
+                        // TODO Conversation Detail.
+                    }
+                } else {
+                    console.log("error: conversation ack fail. ");
+                }
+                break;
+
+            case com.raven.common.protos.RavenMessage.Type.HisMessagesAck:
+                let hisAck = msg.hisMessagesAck;
+                if (this.msgMap.has(hisAck.id)) {
+                    original = this.msgMap.get(hisAck.id);
+                    this.msgMap.delete(hisAck.id);
+                    //TODO History Message Insert.
+                } else {
+                    console.log("error: map not contain " + hisAck.id);
+                }
+                break;
+            case com.raven.common.protos.RavenMessage.Type.UpDownMessage:
+                console.log("new incoming messages.");
+                //DB insert  Chat List insert, Conversation list update.
+                
+                break;
+            }
     }
 
     public send(message: com.raven.common.protos.RavenMessage): void {
@@ -109,8 +156,10 @@ export class SocketService {
         }
     }
 
-    // Messages 
-    
+    /*
+     * Messages 
+     */
+
     // Login message
     private login(): void {
         let login = com.raven.common.protos.Login.create({
@@ -153,6 +202,103 @@ export class SocketService {
             heartBeat: pong
         });
         this.messages.next(message);
+    }
+
+    // Conversation Request. ALL
+    private getAllConversationList(id: number): void {
+        let request = com.raven.common.protos.ConverReq.create({
+            id: id, 
+            type: com.raven.common.protos.OperationType.ALL
+        });
+        let message = com.raven.common.protos.RavenMessage.create({
+            type: com.raven.common.protos.RavenMessage.Type.ConverReq,
+            converReq: request
+        });
+
+        return this.send(message);
+    }
+    
+    // Conversation Request. DETAIL
+    private getDetailConversationList(id: number, cid: string): void {
+        let request = com.raven.common.protos.ConverReq.create({
+            id: id, 
+            type: com.raven.common.protos.OperationType.DETAIL,
+            conversationId: cid
+        });
+        let message = com.raven.common.protos.RavenMessage.create({
+            type: com.raven.common.protos.RavenMessage.Type.ConverReq,
+            converReq: request
+        });
+
+        return this.send(message);
+    }
+    
+    //Single Message.
+    private sendSingleMessage(id: number, fromId: string, targetId: string, 
+        type: com.raven.common.protos.MessageType, content: string, convId: string): void {
+        let msgContent = com.raven.common.protos.MessageContent.create({
+            uid: fromId,
+            type: type,
+            content: content,
+            time: new Date().getTime()
+        });
+
+        let upMsg = com.raven.common.protos.UpDownMessage.create({
+            cid: id, 
+            fromUid: fromId,
+            targetUid: targetId,
+            converType: com.raven.common.protos.ConverType.SINGLE,
+            converId: convId != null ? convId: "",
+            content: msgContent
+        });
+        let message = com.raven.common.protos.RavenMessage.create({
+            type: com.raven.common.protos.RavenMessage.Type.UpDownMessage,
+            upDownMessage: upMsg
+        });
+
+        return this.send(message);
+    }
+    
+    //Group Message.
+    private sendGroupMessage(id: number, fromId: string, targetId: string, 
+        type: com.raven.common.protos.MessageType, content: string, groupId: string, convId: string): void {
+        let msgContent = com.raven.common.protos.MessageContent.create({
+            uid: fromId,
+            type: type,
+            content: content,
+            time: new Date().getTime()
+        });
+
+        let upMsg = com.raven.common.protos.UpDownMessage.create({
+            cid: id, 
+            fromUid: fromId,
+            targetUid: targetId,
+            converType: com.raven.common.protos.ConverType.GROUP,
+            converId: convId != null ? convId: "",
+            content: msgContent,
+            groupId: groupId
+        });
+        let message = com.raven.common.protos.RavenMessage.create({
+            type: com.raven.common.protos.RavenMessage.Type.UpDownMessage,
+            upDownMessage: upMsg
+        });
+
+        return this.send(message);
+    }
+    
+    // get Message List.
+    private getMessageList(id: number, convId: string, beginTime: number): void {
+        let request = com.raven.common.protos.HisMessagesReq.create({
+            id: id, 
+            converId: convId,
+            beaginTime: beginTime
+        });
+        let message = com.raven.common.protos.RavenMessage.create({
+            type: com.raven.common.protos.RavenMessage.Type.HisMessagesReq,
+            hisMessagesReq: request
+        });
+
+        return this.send(message);
     }
 
 }
