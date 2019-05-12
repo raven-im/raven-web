ad94fd999dae9a9074d422c0435a61f60a08047b
baibing <baibing@rongcloud.cn>
2019-05-12 18:49:40 +0800

    add chat detail component

---
+++ src/app/chat/chat.detail.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, ViewChildren, ViewChild, AfterViewInit, QueryList, ElementRef } from '@angular/core';
+import { MatDialog, MatDialogRef, MatList, MatListItem } from '@angular/material';
+
+import { Action } from './shared/model/action';
+import { Event } from './shared/model/event';
+// import { Message } from './shared/model/message';
+import { User } from './shared/model/user';
+import { SocketService } from './shared/services/socket.service';
+import { DialogUserComponent } from './dialog-user/dialog-user.component';
+import { DialogUserType } from './dialog-user/dialog-user-type';
+import { com } from 'assets/message';
+import { ActivatedRoute } from '@angular/router';
+
+
+const AVATAR_URL = 'https://api.adorable.io/avatars/285';
+
+@Component({
+  selector: 'tcc-chat-detail',
+  templateUrl: './chat.detail.component.html',
+  styleUrls: ['./chat.detail.component.css']
+})
+export class ChatDetailComponent implements OnInit, AfterViewInit {
+  action = Action;
+  user: User;
+  messages: com.raven.common.protos.RavenMessage[] = [];
+  messageContent: string;
+  ioConnection: any;
+  dialogRef: MatDialogRef<DialogUserComponent> | null;
+  defaultDialogUserParams: any = {
+    disableClose: true,
+    data: {
+      title: 'Welcome',
+      dialogType: DialogUserType.LOGIN
+    }
+  };
+
+
+  // getting a reference to the overall list, which is the parent container of the list items
+  @ViewChild(MatList, { read: ElementRef }) matList: ElementRef;
+
+  // getting a reference to the items/messages within the list
+  @ViewChildren(MatListItem, { read: ElementRef }) matListItems: QueryList<MatListItem>;
+
+  constructor(private socketService: SocketService,
+    private route: ActivatedRoute,
+    public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    const targetId = this.route.snapshot.paramMap.get('id');
+    console.log("Detail init " + targetId);
+    this.initModel();
+  }
+
+  ngAfterViewInit(): void {
+    // subscribing to any changes in the list of items / messages
+    this.matListItems.changes.subscribe(elements => {
+      this.scrollToBottom();
+    });
+  }
+
+  // auto-scroll fix: inspired by this stack overflow post
+  // https://stackoverflow.com/questions/35232731/angular2-scroll-to-bottom-chat-style
+  private scrollToBottom(): void {
+    try {
+      this.matList.nativeElement.scrollTop = this.matList.nativeElement.scrollHeight;
+    } catch (err) {
+    }
+  }
+
+  private initModel(): void {
+    const randomId = this.getRandomId();
+    this.user = {
+      id: randomId,
+      avatar: `${AVATAR_URL}/${randomId}.png`
+    };
+  }
+
+  private initIoConnection(): void {
+    this.socketService.initSocket();
+  }
+
+  private getRandomId(): number {
+    return Math.floor(Math.random() * (1000000)) + 1;
+  }
+
+  public onClickUserInfo() {
+    this.openUserPopup({
+      data: {
+        username: this.user.name,
+        title: this.user.name == null ? 'Welcome' : 'Profile',
+        dialogType: this.user.name == null ? DialogUserType.LOGIN : DialogUserType.LOGOUT
+      }
+    });
+  }
+
+  private openUserPopup(params): void {
+    this.dialogRef = this.dialog.open(DialogUserComponent, params);
+    this.dialogRef.afterClosed().subscribe(paramsDialog => {
+      if (!paramsDialog) {
+        return;
+      }
+
+      this.user.name = paramsDialog.username;
+      if (paramsDialog.dialogType === DialogUserType.LOGIN) {
+        this.initIoConnection();
+      } else if (paramsDialog.dialogType === DialogUserType.LOGOUT) {
+      }
+    });
+  }
+
+  public sendMessage(message: string): void {
+    if (!message) {
+      return;
+    }
+
+    // this.socketService.send({
+    //   from: this.user,
+    //   content: message
+    // });
+    this.messageContent = null;
+  }
+}
