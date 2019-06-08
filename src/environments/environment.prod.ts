e889e33442c2fd98658a2acb676748cf03dbb6f7
baibing <baibing@rongcloud.cn>
2019-06-08 22:45:57 +0800

    adapt to new prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  APP_SERVER_URL:'http://34.80.127.96:8080/api',
-  IM_SERVER_URL:'http://34.80.127.96:8060/raven-zuul/route',
+  APP_SERVER_URL:'http://114.67.79.183:8080/api',
+  IM_SERVER_URL:'http://114.67.79.183:8060/raven-zuul/route',
 };
