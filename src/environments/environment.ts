d99ad18477d65bbf7290d08aadc91db0d5c89a58
baibing <baibing@rongcloud.cn>
2019-06-11 23:42:17 +0800

    update chat list items

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   IM_SERVER_IP:'114.67.79.183',
-  APP_SERVER_URL:'https://114.67.79.183:8080/api',
-  IM_SERVER_URL:'https://114.67.79.183:8060/raven-zuul/route',
+  APP_SERVER_URL:'http://114.67.79.183:8080/api',
+  IM_SERVER_URL:'http://114.67.79.183:8060/raven-zuul/route',
 };
